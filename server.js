156b5fad5edc0597dc84f3bfabfe1363a93c6ca6
Abrams, Donald <dabrams@cardinalsolutions.com>
2014-10-29 13:15:05 -0400

    fix css image loading in dev

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(compress());
 app.use(livereload());
 app.use('/dev/bower_components', express.static(__dirname + "/bower_components"));
 app.use('/dev/images', express.static(__dirname + "/assets/images"));
+app.use('/dev/assets/css/images', express.static(__dirname + "/assets/images"));
 app.use('/dev', express.static(__dirname + "/dev_target"));
 app.use('/prod', express.static(__dirname + "/target"));
 app.get('/', function(req, res) {
